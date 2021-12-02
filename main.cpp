cf31cd1a029988de7e0e20c5e13e887124483d9c
Marouane Ougnou <mougnou@e2r13p8.1337.ma>
2021-12-02 19:13:45 +0100

    First commit

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include "vector.hpp"
+
+int main()
+{
+	ft::vector<int> abc;
+
+	std::cout << abc.ab << std::endl;
+}
---
+++ vector.hpp
@@ -0,0 +1,23 @@
+#ifndef VECTOR_HPP
+#define VECTOR_HPP
+#include <iostream>
+
+namespace ft
+{
+
+	template <typename T>
+	class vector
+	{
+		public:
+			int ab;
+			vector()
+			{
+				ab = 1;
+			}
+
+
+
+	};
+}
+
+#endif
