f4e25294cda525112eab3a6a25efcba3be7b30f3
Marouane Ougnou <mougnou@e1r12p4.1337.ma>
2022-03-04 16:54:11 +0100

    Finished Vector

--- main.cpp
+++ main.cpp
@@ -7,115 +7,13 @@
 #define EQUAL(x) ((x) ? (std::cout << "\033[1;32mAC\033[0m\n") : (std::cout << "\033[1;31mWA\033[0m\n"))
 
 
-// template<typename Input>
-// void test(Input a,typename std::enable_if<!std::is_integral<Input>::value,
-//                                   Input>::type* = nullptr)
-// {
-// 	std::cout << "input" << std::endl;
-// 	std::cout << a[1] << std::endl;
-// }
 
-//! Fix Assign and Insert with Enable_if (Input Iterator)
 //! Implement istream iterator with insert range and assign range
 int main()
 {
-
-	//! Fix resize
 	
 
-	// std::vector<int> v1(arr,arr+9);
-	// std::vector<int>::reverse_iterator it(v1.rend());
 
-	// std::cout << *it << std::endl;
-
-
-	
-	
-		std::string str, ft_str;
-
-		ft::vector<std::string>::size_type s, ft_s;
-		ft::vector<std::string>::size_type c, ft_c;
-		ft::vector<std::string>::iterator b1, b2;
-		
-		bool cond;
-		
-		std::vector<std::string> v(200, "hello");
-		std::vector<std::string> v1(200, "string");
-		ft::vector<std::string> ft_v(200, "hello");
-		ft::vector<std::string> ft_v1(200, "string");
-
-		b1 = ft_v.begin();
-		b2 = ft_v1.begin();
-		
-		swap(v, v1);
-		swap(ft_v, ft_v1);
-		// std::cout << "b2 " << (&*b2) << std::endl;
-		// std::cout << "b2 " << (&*(ft_v1.begin())) << std::endl;
-
-		s = v.size();
-		ft_s = ft_v.size();
-		c = v.capacity();
-		ft_c = ft_v.capacity();
-		for (size_t i = 0; i < v.size(); ++i)
-			str += v[i];
-		for (size_t i = 0; i < ft_v.size(); ++i)
-			ft_str += ft_v[i];
-		
-		cond = ((str == ft_str) && (s == ft_s) && (c == ft_c));
-		cond = (cond && ((&*b2) == (&(*ft_v.begin()))));
-		std::cout << "cond 1: " << cond << std::endl;
-		cond = (cond && ((&*b1) == (&(*ft_v1.begin()))));
-	
-		// EQUAL(cond);
-
-		// std::cout << "Valid_it add: " << &(*valid_it) << std::endl;
-		// // std::cout << "begin add: " << &(*ft_v.begin())<< std::endl;
-		// std::cout << "after insert valid_it: " << &v[0] << std::endl;
-		// std::cout << "my begin: " << &(*ft_v.begin()) << std::endl;
-		std::cout << "My Size: " << ft_s << std::endl;
-		std::cout << "Their size: " << s << std::endl;
-		std::cout << "My capacity: " << ft_c << std::endl;
-		std::cout << "Their capacity: " << c << std::endl;
-		// std::cout << "My string: " << ft_str << std::endl;
-		// std::cout << "Their string: " << str << std::endl;
-		if (ft_str == str)
-			std::cout << "Strings are equal" << std::endl;
-		else
-			std::cout << "Strings are not equal" << std::endl;
-		std::cout << "my string length: " << ft_str.length() << std::endl;
-		std::cout << "Their String length : " << str.length() << std::endl;
-		// std::cout << "My iterator: " << *ft_it << std::endl;
-		// std::cout << "Their iterator: " << *it << std::endl;
-		
-		// std::cout << "cond: " << cond << std::endl;
-	
-
-	// for(int i = 0; i < 10; i++)
-	// {
-	// 	std::cout << ft_v1[i] << std::endl;
-	// }
-	// ft_v1.reserve(51);
-	// ft_v1.assign(100, "string1");	
-	// for (; ft_valid_it != ft_valid_eit; ft_valid_it++)
-	// 		ft_sit1 += *ft_valid_it;
-
-
-
-	//ft::vector<int> data((std::istream_iterator<int>(std::cin)),std::istream_iterator<int>());
-	//enter + ctrl D
-	// for (int i = 1; i < 10;i++)
-	// 	abc.push_back(i);
-	// abc.insert(abc.begin() + 2, 40,3);
-	// std::cout << *it << std::endl;
-	// if (it == it2)
-	// 	std::cout << "true" << std::endl;
-	
-	// data.insert(it,abc.begin(),abc.end());
-	// it = it - 1;
-
-	// for(size_t i = 0; i < data.size();i++)
-	// 	std::cout << data[i] << std::endl;
-	// std::cout << "Size: " << data.size() << " Capacity: " << data.capacity() << std::endl;
 	// system("leaks a.out");
 }
 
